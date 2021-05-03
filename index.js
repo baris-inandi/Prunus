e0d6ff362468c83309503ed1fe83e7a4b163b7f7
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-05-03 13:00:09 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function goToByScroll(id) {
+    // Remove "link" from the ID
+    id = id.replace("link", "");
+    // Scroll
+    $('html,body').animate({
+        scrollTop: $("#" + id).offset().top
+    }, 'slow');
+}
